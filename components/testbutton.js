e6a4c6b89d12c2322ecc4745bfd8ddb42ae550d7
hazemshikhon <37372449+hazemshikhon@users.noreply.github.com>
2019-11-20 20:27:42 +0200

    Add test button as comp. and indiactor for test
    this button for four chooses and indicator will include two screen first enter to test page and second page will include Qes

---
+++ components/testbutton.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+  } from 'react-native';
+
+  export default class TestButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        color: 'white',
+        answer: ''
+      }
+  
+    }
+  
+    componentDidMount() {
+      this.setState({ answer: this.props.answer })
+  
+      console.log('props: ', this.props);
+  
+    }
+  
+    renderAns(answerType) {
+      const { answer } = this.state;
+      return (
+        <TouchableOpacity style={{ width: 120, borderRadius: 25, borderWidth: 2.5, borderColor: '#ae71f2', backgroundColor: answer === answerType ? "#ae71f2" : 'white' }}
+          onPress={() => {
+            this.setState({ answer: answerType })
+            // this.props.onSelected('never')
+  
+          }}>
+          <Text style={{ paddingVertical: 3, paddingHorizontal: 3, textAlign: 'center', fontSize: 20, color: 'black' }}>
+            {answerType}
+          </Text>
+        </TouchableOpacity>
+      )
+    }
+  
+    render() {
+      return (
+        <View style={{ flexDirection: 'column'}}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+  
+            {this.renderAns("Never")}
+            {this.renderAns("Rarely")}
+          </View>
+  
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 15 }}>
+  
+          {this.renderAns("Often")}
+            {this.renderAns("Always")}
+          </View>
+        </View>  
+      );
+  
+    }
+  }
---
+++ screens/testindicat.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Dimensions,
+    BackHandler,
+    AsyncStorage
+} from 'react-native';
+
+export default class IndicatorForTest extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 0,
+            text: 'Next',
+            display: 'entertotest',
+        }
+    }
+
+    componentDidMount() {
+        this.backHandler = BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+    }
+
+    componentWillUnmount() {
+        this.backHandler.remove()
+    }
+
+    handleBackPress = () => {
+        if (this.state.display === 'entertotest') {
+            BackHandler.exitApp()
+        } else if (this.state.display === 'One') {
+            this.setState({ display: 'entertotest' })
+        }
+        else if (this.state.display === 'Two') {
+            this.setState({ display: 'One' })
+        }
+        else if (this.state.display === 'Three') {
+            this.setState({ display: 'Two' })
+        }
+        else if (this.state.display === 'Four') {
+            this.setState({ display: 'Three' })
+        }
+        return true;
+    }
+
+    renderPage = () => {
+        switch (this.state.display) {
+            case 'entertotest':
+                return <EnterToTest />
+            case 'One':
+                return <One />
+            case 'Two':
+                return <Two />
+            case 'Three':
+                return <Three />
+            case 'Four':
+                return <Four />
+        }
+    }
+
+
+
+    registerPressed = () => {
+        if (this.state.display === 'entertotest') {
+            this.setState({ display: 'One' })
+        } else if (this.state.display === 'One') {
+            this.setState({ display: 'Two' })
+        }
+
+    }
+
+    render() {
+        return (
+            <View style={{}}>
+                <LinearGradient
+                    colors={['#5653e2', '#795EE3', '#ae71f2']}
+                    style={styles.linearGradient}>
+
+                    <Text style={{ textAlign: 'center', fontWeight: 'bold', fontSize: 27, marginTop: 15, color: 'white', marginTop: 20 }}> PERSONALITY TEST </Text>
+
+                    <View style={styles.dynamicScreen}> {this.renderPage()}  </View>
+
+                    <TouchableOpacity style={{ borderRadius: 30, marginTop: 9, width: '70%', marginLeft: 52, flexDirection: 'column' }}
+                        onPress={() => { this.registerPressed() }}>
+                        <LinearGradient
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0 }}
+                            colors={['#5653e2', '#795EE3', '#862deb']}
+                            style={styles.linearGradient2}>
+                            <Text style={{ color: '#ffffff', fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginTop: 9 }}>Next</Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                </LinearGradient>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    dynamicScreen: {
+        backgroundColor: 'white',
+        marginTop: 10,
+        height: height * .77,
+        borderRadius: 18
+
+    },
+    linearGradient: {
+
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5,
+        height: '100%'
+    },
+    linearGradient2: {
+
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 15,
+        height: 40
+
+    }
+
+});
+
